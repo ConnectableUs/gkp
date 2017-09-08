a91a704cf6e4aa087336176958928cc53a0f999a
Yarko Tymciurak <yarkot1@gmail.com>
2017-09-08 16:53:14 -0500

    make updates a little simpler;

---
+++ new-archive-db.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# grab a new google keep archive:
+
+# 1/ put it in a clean directory:
+
+DEST=./Takeout
+
+rm -rf $DEST/*
+
+# unpack the latest archive
+
+SRCD=my-google-data-downloads
+SRC="$SRCD/$(ls -rt $SRCD | tail -1)"
+
+tar xzf $SRC
+
+# finally, make a new DB from this:
+
+
+# make sure we're in venv; if not, make it so:
+which python | fgrep env/keep/bin/python > /dev/null
+if [ "$?" -ne "0" ]; then
+    source env/keep/bin/activate
+fi
+
+python keep2db.py
